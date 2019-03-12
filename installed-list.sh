185d571f87e2b0bca79600187cbbe117c7fb9081
Rick O'Sullivan <rick.osullivan@patrodyne.org>
2019-03-12 14:03:53 -0400

    Added sample configurations and test scripts.

---
+++ installed-list.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+ls -l /etc/default/pound
+ls -l /etc/pound/
+ls -l /etc/rsyslog.d/30-pound.conf
+ls -l /usr/local/share/man/man8/pound.8
+ls -l /usr/local/share/man/man8/poundctl.8
+ls -l /usr/local/bin/poundctl
+ls -l /usr/local/sbin/pound
+ls -l /var/chroot/pound/
+ls -l /var/log/pound/
+ls -l /var/run/pound/poundctl.socket
