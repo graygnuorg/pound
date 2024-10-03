2b2e3fa8568ba273cc5c2fca1c353a9874723f2d
Sergey Poznyakoff <gray@gnu.org>
2024-10-03 15:52:27 +0300

    Add missing copyleft statements

--- src/extern.h
+++ src/extern.h
@@ -1,5 +1,21 @@
 /*
- * Global variables needed by everybody
+ * Global variable definitions for pound.
+ *
+ * Copyright (C) 2002-2010 Apsis GmbH
+ * Copyright (C) 2018-2024 Sergey Poznyakoff
+ *
+ * Pound is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Pound is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with pound.  If not, see <http://www.gnu.org/licenses/>.
  */
 
 extern char const *progname;          /* program name */
--- src/mem.h
+++ src/mem.h
@@ -1,3 +1,20 @@
+/* This file is part of pound
+ * Copyright (C) 2020-2024 Sergey Poznyakoff
+ *
+ * Pound is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Pound is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with pound.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 void *mem2nrealloc (void *p, size_t *pn, size_t s);
 void xnomem (void);
 void *xmalloc (size_t s);
