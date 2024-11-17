bef9b450ee502f6faf72c80cc3c53e26895e1988
Sergey Poznyakoff <gray@gnu.org>
2024-11-17 20:39:41 +0200

    Lua: Implement "pound" table.
    So far it contains one function: "log" and 9 integer constants.

--- src/pndlua.c
+++ src/pndlua.c
@@ -21,23 +21,23 @@
 #include "lauxlib.h"
 #include "lualib.h"
 
-static lua_State *p_lua_state;
-static pthread_mutex_t p_lua_state_mutex = PTHREAD_MUTEX_INITIALIZER;
+static lua_State *pndlua_state;
+static pthread_mutex_t pndlua_state_mutex = PTHREAD_MUTEX_INITIALIZER;
 
 static int
 pndlua_load (char const *fname)
 {
   int res;
 
-  if (luaL_loadfile (p_lua_state, fname) != LUA_OK)
+  if (luaL_loadfile (pndlua_state, fname) != LUA_OK)
     {
       logmsg (LOG_ERR, "error loading Lua file %s: %s", fname,
-	      lua_tostring (p_lua_state, -1));
-      lua_pop (p_lua_state, 1);
+	      lua_tostring (pndlua_state, -1));
+      lua_pop (pndlua_state, 1);
       return -1;
     }
 
-  res = lua_pcall (p_lua_state, 0, LUA_MULTRET, 0);
+  res = lua_pcall (pndlua_state, 0, LUA_MULTRET, 0);
   switch (res)
     {
     case LUA_OK:
@@ -45,8 +45,8 @@ pndlua_load (char const *fname)
 
     case LUA_ERRRUN:
       logmsg (LOG_ERR, "Lua runtime error in %s: %s",
-	      fname, lua_tostring (p_lua_state, -1));
-      lua_pop (p_lua_state, 1);
+	      fname, lua_tostring (pndlua_state, -1));
+      lua_pop (pndlua_state, 1);
       return -1;
 
     case LUA_ERRMEM:
@@ -55,14 +55,14 @@ pndlua_load (char const *fname)
 
     case LUA_ERRERR:
       logmsg (LOG_ERR, "Lua message handler error in %s: %s",
-	      fname, lua_tostring (p_lua_state, -1));
-      lua_pop (p_lua_state, 1);
+	      fname, lua_tostring (pndlua_state, -1));
+      lua_pop (pndlua_state, 1);
       return -1;
 
     case LUA_ERRGCMM:
       logmsg (LOG_ERR, "Lua garbage collector error in %s: %s",
-	      fname, lua_tostring (p_lua_state, -1));
-      lua_pop (p_lua_state, 1);
+	      fname, lua_tostring (pndlua_state, -1));
+      lua_pop (pndlua_state, 1);
       return -1;
 
     default:
@@ -80,35 +80,98 @@ pndlua_match (POUND_HTTP *phttp, char const *fname, int argc, char **argv)
   int i;
   int res;
 
-  pthread_mutex_lock (&p_lua_state_mutex);
+  pthread_mutex_lock (&pndlua_state_mutex);
 
-  lua_getglobal (p_lua_state, fname);
+  lua_getglobal (pndlua_state, fname);
   for (i = 0; i < argc; i++)
-    lua_pushstring (p_lua_state, argv[i]);
+    lua_pushstring (pndlua_state, argv[i]);
 
-  res = lua_pcall (p_lua_state, argc, 1, 0);
+  res = lua_pcall (pndlua_state, argc, 1, 0);
   if (res)
     {
       logmsg (LOG_ERR, "(%"PRItid") error calling Lua function %s: %s",
-	      POUND_TID (), fname, lua_tostring (p_lua_state, -1));
+	      POUND_TID (), fname, lua_tostring (pndlua_state, -1));
       res = -1;
     }
   else
     {
-      res = lua_toboolean (p_lua_state, -1);
+      res = lua_toboolean (pndlua_state, -1);
     }
 
-  lua_pop (p_lua_state, 1);
+  lua_pop (pndlua_state, 1);
 
-  pthread_mutex_unlock (&p_lua_state_mutex);
+  pthread_mutex_unlock (&pndlua_state_mutex);
   return res;
 }
+
+static void
+check_args (lua_State *s, char *fname, int nargs)
+{
+  if (lua_gettop (s) == nargs)
+    return;
+  luaL_error(s, "'%s' requires %d arguments", fname, nargs);
+}
+
+static int
+pndlua_pound_log (lua_State *s)
+{
+  char const *msg;
+  int prio;
+
+  check_args (s, "log", 2);
+  prio = luaL_checkinteger (s, 1);
+  msg = luaL_checkstring (s, 2);
+  logmsg (prio, "%s", msg);
+  return 0;
+}
+
+
+typedef int (*PND_LUAFUNC) (lua_State *);
+
+static void
+pndlua_dcl_function (lua_State *s, char const *name, PND_LUAFUNC func)
+{
+  lua_pushstring (s, name);
+  lua_pushcfunction (s, func);
+  lua_rawset (s, -3);
+}
+
+static void
+pndlua_dcl_integer (lua_State *s, char const *name, int value)
+{
+  lua_pushstring (s, name);
+  lua_pushinteger (s, value);
+  lua_rawset (s, -3);
+}
+
+static struct kwtab severity_table[] = {
+  { "EMERG",   LOG_EMERG },
+  { "ALERT",   LOG_ALERT },
+  { "CRIT",    LOG_CRIT },
+  { "ERR",     LOG_ERR },
+  { "WARNING", LOG_WARNING },
+  { "NOTICE",  LOG_NOTICE },
+  { "INFO",    LOG_INFO },
+  { "DEBUG",   LOG_DEBUG },
+  { NULL }
+};
 
 void
 pndlua_init (void)
 {
-  p_lua_state = luaL_newstate ();
-  luaL_openlibs (p_lua_state);
+  int i;
+
+  pndlua_state = luaL_newstate ();
+  luaL_openlibs (pndlua_state);
+
+  lua_newtable (pndlua_state);
+
+  for (i = 0; severity_table[i].name; i++)
+    pndlua_dcl_integer (pndlua_state,
+			severity_table[i].name, severity_table[i].tok);
+
+  pndlua_dcl_function (pndlua_state, "log", pndlua_pound_log);
+  lua_setglobal (pndlua_state, "pound");
 }
 
 int
